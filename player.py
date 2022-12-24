6fa576bc1b0d45aed8cb695bb003800228a7d6ce
Deven Tseng <devenboss1@gmail.com>
2022-12-23 18:12:52 -0800

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,6 @@
 from board import Board
 import random
 
-invalid_letters = "Input must be a sequence of [WASD]"
-invalid_sequence = "Invalid sequence of moves"
 
 class Player:
     """
@@ -17,44 +15,43 @@ class Player:
 
     Methods:
     --------
-    is_valid_move(board:Board, sequence:string, other:Player) -> bool
+    is_valid_move(board:Board, move:string, other:Player) -> bool
         Returns True if sequence is a valid move
-    is_valid_position(board:Board, other_player:Player): -> bool
+    is_valid_position(other_player:Player): -> bool
         Returns True if current Player's position is valid
     is_player_one() -> bool
         Return True if player is p1
     get_dice_roll() -> int
-        Return the result of a dice roll (1-6), which represents the number of moves
+        Return the result of a dice roll (1-6), which represents the number of moves (TODO)
     """
 
     def __init__(self, player_identity, position):
-        self.player_identity= player_identity
+        self.player_identity = player_identity
         self.position = position
 
-    def is_valid_move(self, board, sequence, other):
-        for character in sequence:
-            if character not in ['W','A','S','D']:
-                print(invalid_letters)
-                return False
+    def get_position(self):
+        return self.position
 
-            if character == 'W':
-                self.position[1] += 1
-            elif character == 'A':
-                self.position[0] -= 1
-            elif character == 'S':
-                self.position[1] -= 1
-            elif character == 'D':
-                self.position[0] += 1
-            if not self.is_valid_position(board, other):
-                print(invalid_sequence)
-                return False
-            
-        return True
+    def set_position(self, position):
+        self.position = position
 
-    def is_valid_position(self, board, other_player):
-        if self.position[0] < 0 or self.position[1] < 0 or self.position[0] > Board.board_size-1 or self.position[1] > Board.board_size - 1:
+    def update_position(self, move):
+        if move == 'W':
+            self.position[1] += 1
+        elif move == 'A':
+            self.position[0] -= 1
+        elif move == 'S':
+            self.position[1] -= 1
+        elif move == 'D':
+            self.position[0] += 1
+
+    def is_valid_position(self, other_player):
+        current_position = self.get_position()
+        # Check for out of bounds
+        if current_position[0] < 0 or current_position[1] < 0 or current_position[0] > Board.board_size-1 or current_position[1] > Board.board_size - 1: 
             return False
-        elif self.position == other_player.position:
+        # Check if square is occupied by other player
+        elif current_position == other_player.get_position():
             return False 
         return True
 
